4587f155f7869cdae298207c7ffb82ff852501a4
Stephen Sennett <ssennett@ssennett.net>
2021-11-17 19:12:14 +1100

    Including bootstrap for lab

---
+++ lab/ec2-instance-bootstrapping/README.md
@@ -0,0 +1,3 @@
+Files for the "EC2 Instance Bootstrapping" lab:
+
+* `webserver-02` is the bootstrapping file required for the lab, stored in GitHub for Challenge Mode
---
+++ lab/ec2-instance-bootstrapping/webserver-02.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install apache2 unzip -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+echo '<html><h1>Bootstrap Demo</h1><h3>Availability Zone: ' > /var/www/html/index.html
+curl http://169.254.169.254/latest/meta-data/placement/availability-zone >> /var/www/html/index.html
+echo '</h3> <h3>Instance Id: ' >> /var/www/html/index.html
+curl http://169.254.169.254/latest/meta-data/instance-id >> /var/www/html/index.html
+echo '</h3> <h3>Public IP: ' >> /var/www/html/index.html
+curl http://169.254.169.254/latest/meta-data/public-ipv4 >> /var/www/html/index.html
+echo '</h3> <h3>Local IP: ' >> /var/www/html/index.html
+curl http://169.254.169.254/latest/meta-data/local-ipv4 >> /var/www/html/index.html
+echo '</h3></html> ' >> /var/www/html/index.html
+apt-get install mysql-server
